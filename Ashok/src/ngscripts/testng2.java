4abc8a8eb057963c6bd0fb628b31b7ae70fc2fa9
DELL <DELL@DESKTOP-AI30N40>
2022-12-08 15:51:36 +0530

    commit

---
+++ Ashok/src/ngscripts/testng2.java
@@ -0,0 +1,15 @@
+package ngscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testng2 {
+	@Test
+	public void test3()
+	{
+		Reporter.log("3",true);
+	}
+
+
+
+}
